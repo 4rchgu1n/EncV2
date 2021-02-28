16351a0f8a6a95424d3b7d57943150ebd09f6d1f
LucioFex <luciano1D2esteban@gmail.com>
2021-02-28 19:22:08 -0300

    refactor: Modification in the import of the tkinter library.
    refactor ---> Now all the tkinter functions and
    methods are called by the 'tk' prefix.
    
    Fundamental change: (from tkinter import *) -> (import tkinter as tk)

--- EncV2.py
+++ EncV2.py
@@ -1,49 +1,49 @@
-from tkinter import *
+import tkinter as tk
 import random
 abecedario = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ"
 
-Ventana = Tk()
+Ventana = tk.Tk()
 Ventana.title("Enc")
 Ventana.config(bg="gray")
 Ventana.geometry("800x800")
 
-Texto1 = Label(Ventana, text="Escribe el texto aquí para encriptarlo", bg="gray")
+Texto1 = tk.Label(Ventana, text="Escribe el texto aquí para encriptarlo", bg="gray")
 Texto1.grid(row=0,column=0)
 
-Espacio1 = Label(Ventana, text="        ",bg="gray")
+Espacio1 = tk.Label(Ventana, text="        ",bg="gray")
 Espacio1.grid(row=0,column=1)
 
-Texto2 = Label(Ventana, text="Escribe el texto aquí para desencriptarlo", bg="gray")
+Texto2 = tk.Label(Ventana, text="Escribe el texto aquí para desencriptarlo", bg="gray")
 Texto2.grid(row=0,column=2)
 
-Enc = StringVar()
+Enc = tk.StringVar()
 Enc.get()
-Entrada1= Entry(Ventana,textvariable=Enc)
+Entrada1= tk.Entry(Ventana,textvariable=Enc)
 Entrada1.grid(row=1,column=0)
 
-DEnc = StringVar()
+DEnc = tk.StringVar()
 DEnc.get()
-Entrada2= Entry(Ventana,textvariable=DEnc)
+Entrada2= tk.Entry(Ventana,textvariable=DEnc)
 Entrada2.grid(row=1,column=2)
 
-Texto4 = Label(Ventana, text="", bg="gray")
+Texto4 = tk.Label(Ventana, text="", bg="gray")
 Texto4.place(x=280,y=50)
 
 def cClave():
     abecedario = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ"
     print("".join(random.sample(abecedario,27)))
-    Texto5 = Label(Ventana, text=abecedario, bg="gray")
+    Texto5 = tk.Label(Ventana, text=abecedario, bg="gray")
     Texto5.grid(row=8,column=2)
 
 
-Clave=StringVar()
-Entrada3 = Entry(Ventana,textvariable=Clave)
+Clave=tk.StringVar()
+Entrada3 = tk.Entry(Ventana,textvariable=Clave)
 Entrada3.grid(row=7,column=2)
 
-opcion = StringVar()
-radioBoton1 = Radiobutton(text="Clave por defecto",variable=opcion,value="defecto",bg="gray")
-radioBoton2 = Radiobutton(text="Selecionar clave",variable=opcion,value="selecionar",bg="gray")
-radioBoton3 = Radiobutton(text="Crear clave aleatoria",variable=opcion,value="crear",command=lambda: cClave(),bg="gray")
+opcion = tk.StringVar()
+radioBoton1 = tk.Radiobutton(text="Clave por defecto",variable=opcion,value="defecto",bg="gray")
+radioBoton2 = tk.Radiobutton(text="Selecionar clave",variable=opcion,value="selecionar",bg="gray")
+radioBoton3 = tk.Radiobutton(text="Crear clave aleatoria",variable=opcion,value="crear",command=lambda: cClave(),bg="gray")
 radioBoton1.grid(row=6,column=0)
 radioBoton2.grid(row=7,column=0)
 radioBoton3.grid(row=8,column=0)
@@ -74,11 +74,11 @@ def Encriptar():
     print(ENC2)
 
     TENC = "".join(ENC2)
-    Texto3 = Label(Ventana, text=TENC, bg="gray")
+    Texto3 = tk.Label(Ventana, text=TENC, bg="gray")
     Texto3.grid(row=3,column=0)
     print(TENC)
 
-Boton1 = Button(Ventana, text="Encriptar", command=lambda:Encriptar())
+Boton1 = tk.Button(Ventana, text="Encriptar", command=lambda:Encriptar())
 Boton1.grid(row=2,column=0)
 
 def Desencriptar():
@@ -106,11 +106,11 @@ def Desencriptar():
     print(DENC2)
 
     TDENC = "".join(DENC2)
-    Texto4 = Label(Ventana, text=TDENC, bg="gray")
+    Texto4 = tk.Label(Ventana, text=TDENC, bg="gray")
     Texto4.grid(row=3,column=2)
     print(TDENC)
 
-Boton1 = Button(Ventana, text="Desencriptar", command=lambda:Desencriptar())
+Boton1 = tk.Button(Ventana, text="Desencriptar", command=lambda:Desencriptar())
 Boton1.grid(row=2,column=2)
 
 Ventana.mainloop()
